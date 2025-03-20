ee5faf2bded1adc50f697354c945ec5a76219132
donaldzy <760463769@qq.com>
2025-03-21 01:55:54 +0800

    stream gRPC

--- actor/actor_stream.py
+++ actor/actor_stream.py
@@ -3,10 +3,11 @@ import threading
 import pickle
 from comm.service_pb2_grpc import LearnerStub
 from comm.service_pb2 import Request, Response
+import time
 
-class ActorStream(threading.Thread):
+class Actor(threading.Thread):
     def __init__(self, env, host, port):
-        super(ActorStream, self).__init__()
+        super(Actor, self).__init__()
         self.env = env
         self.observation = self.env.reset()
 
@@ -16,6 +17,9 @@ class ActorStream(threading.Thread):
 
         self.obs_ready = threading.Event()
         self.obs_ready.set()
+
+        self.count = 0
+        self.record_time = time.time()
 
     def request_stream(self):
         while True:
@@ -31,4 +35,13 @@ class ActorStream(threading.Thread):
                 action = pickle.loads(response.action)
                 self.observation = self.env.step(action)
                 self.obs_ready.set()
+                self.logging()
+
+    def logging(self):
+        self.count += 1
+        now = time.time()
+        if now - self.record_time > 5.0:
+            print(self.count / (now - self.record_time))
+            self.count = 0
+            self.record_time = now
 
